f413c039e9a2edc34d4a7eb76e3b0aef0833a668
Vitor Britto <code@vitorbritto.com.br>
2014-05-24 17:44:14 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-markupify
-=========
+![Markupify Logo](src/markupify.jpg "Markupify")
 
-Extract classes or IDS from your Markup Text
+
+## Welcome
+
+Markupify is a simple method to extract some parts of HTML files.
+
+
+## Installation
+
+```bash
+
+# 1. Clone this repository
+$ git clone git://github.com/vitorbritto/markupify.git
+
+# 2. Place the `markupify.sh` script wherever you want
+
+# 3. Make the script executable
+$ chmod u+x path/to/markupify.sh
+
+```
+
+
+## Usage
+
+    ./markupify.sh [ options ] <LOCAL_FILE, FILE_URL>
+
+
+### Options:
+      -c, --class        output classes
+      -i, -id            output ids
+      -u, --url          output urls from href
+
+
+## Bonus
+
+If you prefer, put the following **alias** inside your `~./.bashrc` file:
+
+    alias mify="bash ~/path/to/script/markupify.sh"
+
+Now, you can simply run:
+
+    mify [ options ] <LOCAL_FILE, FILE_URL>
+
+
+## License
+
+[MIT License](http://vitorbritto.mit-license.org/) © Vitor Britto
---
+++ markupify.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------------------------
+#
+# Program: markupify.sh
+# Author:  Vitor Britto
+#
+# Description:
+#       This is my personal method to extract some parts fo HTML files.
+#
+# Important:
+#       First of all, make this script executable to easily run it.
+#       $ chmod u+x markupify.sh
+#
+# Usage: ./markupify.sh [ options ] <LOCAL_FILE, FILE_URL>
+#
+# Options:
+#       -c, --class        output classes
+#       -i, -id            output ids
+#       -u, --url          output urls from href
+#
+# Example:
+#       ./markupify -c index.html
+#
+#
+# Assumptions:
+#       - The url of interest is a simple one.
+#
+# Alias:
+#       alias markupify="bash ~/path/to/script/markupify.sh"
+#
+# ------------------------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------------
+# | MAIN FUNCTION                                                              |
+# ------------------------------------------------------------------------------
+
+markupify() {
+
+    # List Classes
+    if [[ "${1}" == "-c" || "${1}" == "--class" ]]; then
+        mify_class ${1} ${2}
+    else
+        echo "Sorry, any valid parameter."
+    fi
+
+    # List IDs
+    if [[ "${1}" == "-i" || "${1}" == "--id" ]]; then
+        mify_id ${1} ${2}
+    else
+        echo "Sorry, any valid parameter."
+    fi
+
+    # List href URL
+    if [[ "${1}" == "-u" || "${1}" == "--url" ]]; then
+        mify_href ${1} ${2}
+    else
+        echo "Sorry, any valid parameter."
+    fi
+
+}
+
+mify_class() {
+    echo "→ Extracting"
+    cat ${2} | grep -o class= | cut -d \" -f2
+    exit
+}
+
+mify_id() {
+    echo "→ Extracting"
+    cat ${2} | grep id= | cut -d \" -f2
+    exit
+}
+
+mify_href() {
+    echo "→ Extracting"
+    cat ${2} | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d'
+    exit
+}
+
+# Extract classes or IDs from file or URL
+markupify $*
